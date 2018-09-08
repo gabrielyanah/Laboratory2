b897e8e711fc10fabb06a178039f716c98d0e560
flavian <2015086506@ust-ics.mygbiz.com>
2018-09-08 16:45:43 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/iicsust/hernandez/allianagabriel/laboratory2/MainActivity.java
@@ -0,0 +1,31 @@
+package ph.edu.iicsust.hernandez.allianagabriel.laboratory2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.constraints);
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.relative);
+//    }
+}
